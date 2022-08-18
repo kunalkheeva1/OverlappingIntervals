c10629346fe7e58c043ad1368dfa17af433c837e
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-08-17 23:29:32 -0700

    coded the method to merge the intervals with same elements if not adding them to list(which is later converted into an array to be returned)

--- src/OverlappingIntervals.java
+++ src/OverlappingIntervals.java
@@ -1,5 +1,33 @@
+import java.util.*;
+
+
 public class OverlappingIntervals {
+    static int[][] overlappingIntervals(int[][]arr){
+        List<int[]> list = new ArrayList<>();
+        if(arr.length ==0){
+            return list.toArray(new int[0][0]);
+        }
+        Arrays.sort(arr, (a,b)->a[0]-b[0]);
+        int start = arr[0][0];
+        int end = arr[0][1];
+
+        for(int[] i :arr){
+            if(i[0]<= end){
+                end = Math.max(end, i[1]);
+            }
+            else{
+                list.add(new int []{start,end});
+                start = i[0];
+                end = i[1];
+            }
+        }list.add(new int[]{start, end});
+        return list.toArray(new int[0][0]);
+
+
+
+
+    }
     public static void main(String[] args) {
-        
+
     }
 }
